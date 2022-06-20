af85ba795d2105554455b3e4461b798a2d4a3ad9
MohamedGado <boogado@yahoo.com>
2022-06-20 17:37:36 +0200

    project starter

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
+import { colors } from "./src/utils/colors";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Easy as fck</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+  text: {
+    color: "#fff",
   },
 });
---
+++ src/utils/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+  white: "#fff",
+  black: "#000",
+  gray: "#ccc",
+  lightGray: "#eee",
+  darkGray: "#999",
+  red: "#f00",
+  green: "#0f0",
+  blue: "#00f",
+  yellow: "#ff0",
+  orange: "#f60",
+  darkBlue: "#252250",
+};
